2c83c10abd28046450adc84e40f868592ffdb208
capputino <eisbaer1832@gmail.com>
2025-07-07 18:10:24 +0200

    init 2

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs');
+const url = require('url');
+const bodyParser = require('body-parser');
+const { json } = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const favicon = require('serve-favicon');
+const { Server } = require("socket.io");
+const eiows = require("eiows");
+const http = require('http');
+const $ = require('jquery');
+
+let erledigtCounter = 0
+let BestellungCounter = 0
+
+
+//http
+const app = express();
+
+//websocket
+const io = new Server(9877, { cors: { origin: '*', credentials: true }});
+
+const port = 9876;
+
+
+//app.use(favicon(path.join(__dirname, '/', 'favicon.ico')));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use('/public',express.static('public'));
+
+app.get('/', (_, res) => {res.sendFile('/public/html/wikirunner.html', {root: __dirname })});
+  
+app.get('/mix', (_, res) => {res.sendFile('/public/html/results.html', {root: __dirname })});
+
+app.listen(port, () => {console.log(`App listening on port ${port}!`)});
+
+
+let orders = [[],[]];
+
+io.on("connection", (socket) => {
+  console.log("-----------------------");
+  console.log("LOGGING FOR SCOKET: " + socket.id);
+  io.emit("orders", {"orderItems": orders, "done": erledigtCounter})
+
+  socket.on('ordering', (item, username) => {
+    orders[0].push(username)
+    orders[1].push(item)
+    console.log(`User ${username} ordered: ${item}`);
+    io.emit("orders", {"orderItems": orders, "done": erledigtCounter})
+  }); 
+
+  socket.on('finish', (item) => {
+    orders[0].splice(item, 1)
+    orders[1].splice(item, 1)
+    erledigtCounter++
+    io.emit("orders", {"orderItems": orders, "done": erledigtCounter})
+    
+  }); 
+});
