e02c5b8cb2edbe750c3b274244218b1c23a738fb
eisbaer1832 <eisbaer1832@gmail.com>
2025-07-07 20:54:39 +0200

    Wikipedia is now accessable through a proxy, in order to check the DOM in the future

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const { Server } = require("socket.io");
 const eiows = require("eiows");
 const http = require('http');
 const $ = require('jquery');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 let erledigtCounter = 0
 let BestellungCounter = 0
@@ -61,4 +64,51 @@ io.on("connection", (socket) => {
     io.emit("orders", {"orderItems": orders, "done": erledigtCounter})
     
   }); 
+});
+
+
+
+// Main HTML Proxy
+app.get('/proxy', async (req, res) => {
+  const targetUrl = req.query.url;
+  try {
+    const response = await axios.get(targetUrl);
+    const $ = cheerio.load(response.data);
+    const baseUrl = new URL(targetUrl);
+
+    // Rewrite links to CSS/JS/imgs
+    $('link[href], script[src], img[src]').each((_, el) => {
+      const attr = el.name === 'link' || el.name === 'img' ? 'href' : 'src';
+      const original = $(el).attr(attr);
+      if (original && !original.startsWith('data:')) {
+        const absolute = new URL(original, baseUrl).toString();
+        $(el).attr(attr, `/proxy/resource?url=${encodeURIComponent(absolute)}`);
+      }
+    });
+
+    $('a').each(function () {
+    var $this = $(this),
+        href = $this.attr('href');
+    $this.attr('href', "http://127.0.0.1:9876/proxy?url=https://de.wikipedia.org/" + href);
+})
+
+    res.send($.html());
+  } catch (err) {
+    res.status(500).send('Proxy HTML error');
+  }
+});
+// Asset Proxy (CSS, JS, images)
+app.get('/proxy/resource', async (req, res) => {
+  const assetUrl = req.query.url;
+  if (!assetUrl) return res.status(400).send('Missing ?url param');
+  try {
+    const response = await axios.get(assetUrl, {
+      responseType: 'arraybuffer', // handle binary data like images
+    });
+
+    res.set(response.headers); // forward original headers (content-type, etc)
+    res.send(response.data);
+  } catch (err) {
+    res.status(500).send('Proxy resource error');
+  }
 });
---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+wikiFrame = document.getElementById("wikiFrame")
+
+url = wikiFrame.src
+console.log(wikiFrame)
+console.log(url)
